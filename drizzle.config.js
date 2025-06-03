0e3b75765180d1eb35a07c8501cc1efb35c5baf2
harpercheng0049 <69156303+harpercheng0049@users.noreply.github.com>
2025-06-03 11:47:49 +0800

    Revert "feat(db): define user table schema and push to database"

--- drizzle.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  dialect: "postgresql",
-  schema: "./src/configs/schema.js",
-  dbCredentials: {
-    url: "postgresql://neondb_owner:npg_37PsLUwBgqha@ep-damp-mode-a1gs5m0t-pooler.ap-southeast-1.aws.neon.tech/ai-short-video-generator?sslmode=require"
-  },
-});
--- src/configs/schema.js
+++
@@ -1,9 +0,0 @@
-import { pgTable, serial, varchar, boolean } from "drizzle-orm/pg-core";
-
-export const Users = pgTable('user', {
-    id:serial('id').primaryKey(),
-    name:varchar('name').notNull(),
-    email:varchar('email').notNull(),
-    imageUrl:varchar('imageUrl'),
-    subscription:boolean('subscription').default(false)
-})
